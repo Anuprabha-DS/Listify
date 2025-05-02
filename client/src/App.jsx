e173dae4b51ef44dc899609556a8f40a51a63532
Anuprabha-DS <chatwith.anudev@gmail.com>
2025-05-02 13:26:52 +0530

    changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
-import "./App.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login';
 import Register from './Components/Register';
 import NewTask from './Components/Task';
+
+import './App.css'
+
 
 function App() {
   return (
